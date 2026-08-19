d076ddc1843afda8cb10fe88ddc7fac9f8d58341
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2026-08-19 15:55:37 +0200

    feat: pause menu hook

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PauseLayer.hpp>
 
 using namespace geode::prelude;
+
+class $modify(EndPercentagePauseLayer, PauseLayer) {
+
+    void customSetup() {
+        PauseLayer::customSetup();
+
+        log::info("Pause layer");
+    }
+
+};
